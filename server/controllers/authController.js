ce312474ec73167fbc8c3a7f949bb092eb0a30d1
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-12 14:38:16 +0530

    User Register Controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,21 @@
+const User = require('../models/User')
+
 async function registerUser(req, res) {
-  let data = req.body;
-  console.log(data);
-  res.send("success");
+  let { firstName, lastName, username, password } = req.body;
+  // console.log(data);
+  try {
+    const duplicate = await User.find({ username })
+    if (duplicate && duplicate.length > 0) {
+      return res.status(400).send({ message: "User already registerd with this username" })
+    }
+    let user = new User({ firstName, lastName, username, password })
+    const result = await user.save()
+    console.log(result)
+    res.status(201).send({ message: "User registered successfully" })
+  } catch (err) {
+    console.log(err)
+    res.status(400).send(err)
+  }
 }
 
 const AuthController = {
