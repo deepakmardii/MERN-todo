8536891893f9add2adf2eb785be0fb4b902df218
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-12 20:20:53 +0530

    Login and Register Page

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+import login from '../../assets/login.png'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = () => {
+    console.log("login")
+  }
   return (
-    <div>Login</div>
+    <div className='border-2 border-black'>
+      <div>
+        <img src={login} alt="login" className='h-24 w-24' />
+        <h4 className='font-bold uppercase'>Login</h4>
+      </div>
+      <div>
+        <input placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <input placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        New user? <Link to="/register">Register</Link>
+      </div>
+      <button disabled={!username || !password} onClick={handleSubmit}>Login</button>
+    </div>
   )
 }
 
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react'
+import register from '../../assets/login.png'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+  const handleSubmit = () => {
+    console.log("register")
+  }
   return (
-    <div>Register</div>
+    <div className='border-2 border-black'>
+      <div>
+        <img src={register} alt="register" className='h-24 w-24' />
+        <h4 className='font-bold uppercase'>register</h4>
+      </div>
+      <div>
+        <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        <input placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      </div>
+      <div>
+        <input placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <input placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        Exsisting user? <Link to="/login">Login</Link>
+      </div>
+      <button disabled={!username || !password} onClick={handleSubmit}>Register</button>
+    </div>
   )
 }
 
 export default Register
+
