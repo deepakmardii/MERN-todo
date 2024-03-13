994d92bc0e7afbd7e16a8c18c7ae186320c3a9eb
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-13 15:08:18 +0530

    delete and update API

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -11,3 +11,44 @@ exports.createToDo = async (req, res) => {
     console.log(error);
   }
 };
+
+exports.getAllToDo = async (req, res) => {
+  let { userId } = req.params;
+
+  try {
+    const result = await ToDo.find({ createdBy: userId });
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+
+exports.updateToDo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const result = await ToDo.findByIdAndUpdate(
+      id,
+      { $set: data },
+      { returnOriginal: false }
+    );
+    console.log(result);
+    res.send({ message: "ToDO list Updated!" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+
+exports.deleteToDo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await ToDo.findByIdAndDelete(id);
+    console.log(result);
+    res.status(200).send({ message: "ToDo task deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { createToDo } = require("../controllers/todoController");
+const {
+  createToDo,
+  getAllToDo,
+  deleteToDo,
+  updateToDo,
+} = require("../controllers/todoController");
 const authenticationToken = require("../middleware/authJwt");
 const router = express.Router();
 
 router.post("/create-to-do", authenticationToken, createToDo);
+router.get("/get-all-to-do/:userId", authenticationToken, getAllToDo);
+router.delete("/get-all-to-do/:id", authenticationToken, deleteToDo);
+router.patch("/get-all-to-do/:id", authenticationToken, updateToDo);
 
 module.exports = router;
